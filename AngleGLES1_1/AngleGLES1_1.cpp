046c42a9b99951745585a85badc0ae56d1015103
DESKTOP-BC722G9\ga <pavel@generalarcade.com>
2022-11-14 19:49:26 +0700

    Initial commit

---
+++ AngleGLES1_1/AngleGLES1_1.cpp
@@ -0,0 +1,14 @@
+/******************************************************************************
+*
+* Made by pavel@generalarcade.com
+* Test project for rendering using OpenGL ES1.1 via ANGLE with SDL
+* 
+******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# AngleGLES1.1
+Test repository with using SDL and OpenGL ES1.1 via ANGLE
